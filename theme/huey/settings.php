a50ad58c85439b22e334a9a4d04ad9ba2be7d1ee
Cathal O'Riordan <cathal@madein75.com>
2016-10-05 09:44:46 +0100

    Theme Huey initial commit

---
+++ theme/huey/settings.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme More version file.
+ *
+ * @package    theme_huey
+ * @copyright  2016, Waterford Institute of Technology
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die;
+
+if ($ADMIN->fulltree) {
+
+    // Logo file setting.
+    $name = 'theme_huey/logo';
+    $title = get_string('logo', 'theme_huey');
+    $description = get_string('logodesc', 'theme_huey');
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'logo');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // Custom CSS file.
+    $name = 'theme_huey/customcss';
+    $title = get_string('customcss', 'theme_huey');
+    $description = get_string('customcssdesc', 'theme_huey');
+    $default = '';
+    $setting = new admin_setting_configtextarea($name, $title, $description, $default);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+}
