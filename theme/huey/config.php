f9ecc8b8faabaee0e93c12d98dd4185fa2022db7
Cathal O'Riordan <cathal@madein75.com>
2016-10-10 16:56:20 +0100

    add styles for login page

--- theme/huey/config.php
+++ theme/huey/config.php
@@ -97,7 +97,7 @@ $THEME->layouts = array(
         'defaultregion' => 'side-pre',
     ),
     'login' => array(
-        'file' => 'default.php',
+        'file' => 'login.php',
         'regions' => array(),
         'options' => array('langmenu' => true, 'nonavbar' => true),
     ),
