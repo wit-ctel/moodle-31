7c1054fce19615b2faf221bd139dd57fe027603f
Cathal O'Riordan <cathal@madein75.com>
2016-10-19 15:11:21 +0100

    make navbar fixed at top

--- theme/huey/layout/default.php
+++ theme/huey/layout/default.php
@@ -52,7 +52,7 @@ echo $OUTPUT->doctype() ?>
 
 <?php echo $OUTPUT->standard_top_of_body_html() ?>
 
-<nav role="navigation" class="navbar navbar-default">
+<nav role="navigation" class="navbar navbar-default navbar-fixed-top">
     <div class="container-fluid">
     <div class="navbar-header pull-left">
         <?php echo $OUTPUT->navbar_brand(); ?>
