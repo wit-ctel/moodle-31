7c25edfb670fd5aec44e8a5786ed7dacd3c4ad87
Cathal O'Riordan <cathal@madein75.com>
2016-11-11 14:45:04 +0000

    deploy from develop branch to Edge

--- config/deploy/edge.rb
+++ config/deploy/edge.rb
@@ -1,6 +1,6 @@
 set :stage, :edge
 set :deploy_to, "/var/www/edge.moodle.wit.ie/"
-set :branch, ENV["BRANCH_NAME"] || "master"
+set :branch, ENV["BRANCH_NAME"] || "develop"
 
 server 'wit-moodle-web1.heanet.ie', user: 'moodle', roles: %w{ web }
 server 'wit-moodle-web2.heanet.ie', user: 'moodle', roles: %w{ web }
